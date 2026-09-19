3e5e5e1e68311e956edb5b14148eb342efd3cc2d
Boxocean <juandanielfuenmayor@gmail.com>
2026-09-19 19:34:01 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -9,20 +9,22 @@ from routes.lectura import registrar_rutas as lectura_routes
 from routes.seguimiento import registrar_rutas as seguimiento_routes
 from routes.objetivos import objetivos_bp
 
-# Importar Blueprints y el Scheduler en segundo plano
+# Importar Blueprints
 from IA.recomendador import recomendador_bp
 from IA.asistente import ia_bp
-from models.notificaciones import notificaciones_bp, iniciar_scheduler_background
+from models.notificaciones import notificaciones_bp
+from models.recuperacion import recuperacion_bp
 
 app = Flask(__name__)
 
-app.secret_key = "ilovesucklemons" # clave secretosa
+app.secret_key = "ilovesucklemons"
 
 # Registrar Blueprints
 app.register_blueprint(ia_bp)
 app.register_blueprint(recomendador_bp)
 app.register_blueprint(notificaciones_bp)
 app.register_blueprint(objetivos_bp) 
+app.register_blueprint(recuperacion_bp)
 
 CORS(app)
 
@@ -33,11 +35,9 @@ usuarios_routes(app)
 lectura_routes(app)
 seguimiento_routes(app)
 
-# Iniciar el servicio de notificaciones en segundo plano
-iniciar_scheduler_background()
-
 if __name__ == "__main__":
     app.run(
         debug=True,
         port=5000
     )
+    
